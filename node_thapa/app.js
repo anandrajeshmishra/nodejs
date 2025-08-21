695c925a3c5c9f96c05ba42ba7b0ec73e763286a
anandrajeshmishra <anand98425@gmail.com>
2025-08-21 16:50:57 +0530

    work

---
+++ node_thapa/app.js
@@ -0,0 +1,9 @@
+// const add = require("./math");
+// const{add,mult,div,mod,sub,PI} = require("./math");
+const math = require("./math")
+console.log(math.add(2,4));
+console.log(math.mult(2,4));
+console.log(math.div(2,4));
+console.log(math.sub(4,2));
+console.log(math.PI);
+//commonjs
---
+++ node_thapa/math.js
@@ -0,0 +1,19 @@
+const add = (a,b) =>{
+    return a+b;
+};
+const mult = (a,b) =>{
+    return a*b;
+};
+const div = (a,b) =>{
+    return a/b;
+};
+const mod = (a,b) =>{
+    return a%b;
+};
+const sub = (a,b) =>{
+    return a-b;
+};
+const PI = 3.14;
+// module.exports.add = add;
+// module.exports.mult = mult;
+module.exports = {add,mult,div,mod,sub,PI};
